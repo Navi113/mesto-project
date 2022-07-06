05149588698a92b844e17603c6bf280d8568fc30
Ivan Ershov <Navi133@yandex.ru>
2022-07-06 22:02:36 +0300

    Добавлено неактивное состояние кнопки в форме

--- script.js
+++ script.js
@@ -236,8 +236,11 @@ const hasInvalidInput = (inputList) => {
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.classList.add('popup__save-button_disabled');
+    buttonElement.setAttribute('disabled', true);
+
   } else {
     buttonElement.classList.remove('popup__save-button_disabled');
+    buttonElement.removeAttribute('disabled', true);
   }
 }
 
